c24916b56a8b86ac7c6db435d0487b0ce85d09cf
Murugavel <murugavel@murugavel-VirtualBox.(none)>
2013-12-16 14:09:47 -0800

    Till index.html

--- mysite/settings.py
+++ mysite/settings.py
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/murugavel/workspace/django/mysite/templates',
+    '/home/murugavel/workspace/django/poll_tutorial/templates',
 )
 
 INSTALLED_APPS = (
