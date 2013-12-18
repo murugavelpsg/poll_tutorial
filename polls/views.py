b0f5dc6f96bdbab8d0a9516debebe813566638fd
murugavelpsg <murugavelpsg@gmail.com>
2013-12-18 15:13:21 -0800

    voting and results functionality added

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
-from django.http import HttpResponse, Http404
-from polls.models import Poll
-from django.template import Context, loader
-from django.shortcuts import render_to_response
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from polls.models import Choice, Poll
+from django.template import Context, loader, RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the polls index")
@@ -20,11 +21,24 @@ def detail(request, poll_id):
         p = Poll.objects.get(pk=poll_id)
     except:
         raise Http404
-    return render_to_response('polls/detail.html', {'poll':p})
+    return render_to_response('polls/detail.html', {'poll':p}, 
+                              context_instance=RequestContext(request))
 
 def results(request, poll_id):
-    return HttpResponse("You're looking at results of poll %s" % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/results.html', {'poll':p})
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s" % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render_to_response('polls/detail.html', {
+                        'poll':p,
+                        'error_message':"You didn't select a choice",
+                    }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
 
